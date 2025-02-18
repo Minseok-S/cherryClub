8869a3d2234074d81531b687063e113735f03de7
민석 <>
2025-02-18 22:11:11 +0900

    [Feat] 대외사역 추가

--- app/page.tsx
+++ app/page.tsx
@@ -564,7 +564,6 @@ export default function Home() {
             bulletActiveClass:
               "swiper-pagination-bullet-active custom-bullet-active",
           }}
-          navigation={true}
           modules={[Autoplay, Pagination, Navigation]}
           className="w-[90%] md:w-[800px] mx-auto [&_.swiper-pagination-bullet-active]:!bg-red-500 [&_.swiper-pagination-bullet]:!bg-white"
           breakpoints={{
@@ -791,7 +790,6 @@ export default function Home() {
             bulletActiveClass:
               "swiper-pagination-bullet-active custom-bullet-active",
           }}
-          navigation={true}
           modules={[Autoplay, Pagination, Navigation]}
           className="w-[90%] md:w-[800px] mx-auto [&_.swiper-pagination-bullet-active]:!bg-red-500 [&_.swiper-pagination-bullet]:!bg-white"
           breakpoints={{
@@ -903,7 +901,6 @@ export default function Home() {
             bulletActiveClass:
               "swiper-pagination-bullet-active custom-bullet-active",
           }}
-          navigation={true}
           modules={[Autoplay, Pagination, Navigation]}
           className="w-[90%] md:w-[800px] mx-auto [&_.swiper-pagination-bullet-active]:!bg-red-500 [&_.swiper-pagination-bullet]:!bg-white"
           breakpoints={{
@@ -966,9 +963,194 @@ export default function Home() {
         className="pb-20 px-4 min-h-screen flex flex-col items-center justify-center"
         style={{ scrollSnapAlign: "center", scrollMarginTop: "100px" }}
       >
-        <h2 className="text-4xl md:text-6xl font-black text-center mb-12">
+        <h2 className="text-4xl md:text-6xl font-black text-center mb-5 md:mb-7">
           대외사역
         </h2>
+
+        {/* 기본 설명 텍스트 */}
+        <div className="max-w-4xl mx-auto mb-5 md:mb-7 text-center w-full px-4">
+          <p className="text-gray-600 text-sm md:text-base font-medium break-keep whitespace-pre-wrap">
+            체리 동아리는 캠퍼스를 넘어 지역사회와 나라를 섬기는 다양한
+            대외사역에 참여합니다.
+            <br />
+            레드하트 캠페인, My5K, 사랑나눔버스, DMZ 행진 등을 통해 하나님의
+            사랑을 실천합니다.
+          </p>
+        </div>
+
+        {/* 대외사역 설명 */}
+        <div className="w-full max-w-3xl mx-auto mb-4 md:mb-10">
+          <button
+            onClick={() => setShowMovements(!showMovements)}
+            className="w-full flex items-center justify-center space-x-2 bg-white/5 hover:bg-white/10 backdrop-blur-sm p-4 rounded-lg transition-all duration-300"
+          >
+            <h3 className="text-xl md:text-2xl font-bold text-center">
+              대외사역 소개
+            </h3>
+            <svg
+              className={`w-6 h-6 transform transition-transform duration-300 ${
+                showMovements ? "rotate-180" : ""
+              }`}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </button>
+
+          <div
+            className={`transition-all duration-500 overflow-hidden ${
+              showMovements
+                ? "max-h-[1000px] mt-4 opacity-100"
+                : "max-h-0 opacity-0"
+            }`}
+          >
+            {/* PC 버전 */}
+            <div className="hidden md:grid md:grid-cols-2 gap-4">
+              {[
+                {
+                  title: "1221 레드하트",
+                  description:
+                    "12월 21일, 레드하트데이! 나눔으로 웃음과 기쁨이 두 배가 되는 축제의 날. 가족, 친구, 이웃과 함께 즐겨요! 1221 REDHEART DAY.에 참여합니다.",
+                  icon: "❤️",
+                  link: "",
+                },
+                {
+                  title: "My5K",
+                  description:
+                    "내 주변 반경 5㎞ 내에 도움이 필요한 이웃에게 필요를 나누고 섬기며 문화를 선도하여 서로 사랑하는 세상을 함께 만들어 가기 위해 걷기 운동에 참여합니다.",
+                  icon: "🚶",
+                  link: "",
+                },
+                {
+                  title: "5K 사랑나눔버스",
+                  description:
+                    "기독교 문명 개혁운동을 일으키는 NCer Nations-Changer 들이 내가 거주하는 지역, 교회의 반경 5㎞안에 있는 불신자 독거노인을 찾아가서 맞춤섬김에 참여합니다.",
+                  icon: "🚌",
+                  link: "",
+                },
+                {
+                  title: "DMZ 155마일 행진",
+                  description:
+                    "NCMN 통일을 여는 DMZ 155마일 기도행진에 참여합니다.",
+                  icon: "🚶",
+                  link: "https://forms.gle/Kpos1VmM2hfM6LEr6",
+                },
+              ].map((service, index) => (
+                <div
+                  key={index}
+                  className="bg-white/5 backdrop-blur-sm p-4 rounded-lg flex flex-col"
+                >
+                  <div className="flex items-center gap-3 mb-2">
+                    <span className="text-2xl">{service.icon}</span>
+                    <h4 className="font-bold">{service.title}</h4>
+                  </div>
+                  <p className="text-sm text-gray-600 mb-4">
+                    {service.description}
+                  </p>
+                  <div className="mt-auto flex justify-center w-full">
+                    <button
+                      onClick={() => {
+                        if (service.link) {
+                          window.open(service.link, "_blank");
+                        } else {
+                          alert("아직 신청기간이 아닙니다.");
+                        }
+                      }}
+                      className="bg-red-500 text-white py-2 px-4 rounded-full 
+                               hover:bg-red-600 transition-colors duration-300 
+                               text-sm font-bold"
+                    >
+                      참가 신청
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            {/* 모바일 버전 - Swiper */}
+            <div className="md:hidden">
+              <Swiper
+                loop={true}
+                speed={800}
+                spaceBetween={20}
+                centeredSlides={true}
+                pagination={{
+                  clickable: true,
+                  bulletActiveClass:
+                    "swiper-pagination-bullet-active custom-bullet-active",
+                }}
+                modules={[Pagination, Navigation]}
+                className="w-full [&_.swiper-pagination-bullet-active]:!bg-red-500 [&_.swiper-pagination-bullet]:!bg-white"
+              >
+                {[
+                  {
+                    title: "1221 레드하트",
+                    description:
+                      "12월 21일, 레드하트데이! 나눔으로 웃음과 기쁨이 두 배가 되는 축제의 날. 가족, 친구, 이웃과 함께 즐겨요! 1221 REDHEART DAY.에 참여합니다.",
+                    icon: "❤️",
+                    link: "",
+                  },
+                  {
+                    title: "My5K",
+                    description:
+                      "내 주변 반경 5㎞ 내에 도움이 필요한 이웃에게 필요를 나누고 섬기며 문화를 선도하여 서로 사랑하는 세상을 함께 만들어 가기 위해 걷기 운동에 참여합니다.",
+                    icon: "🚶",
+                    link: "",
+                  },
+                  {
+                    title: "5K 사랑나눔버스",
+                    description:
+                      "기독교 문명 개혁운동을 일으키는 NCer Nations-Changer 들이 내가 거주하는 지역, 교회의 반경 5㎞안에 있는 불신자 독거노인을 찾아가서 맞춤섬김에 참여합니다.",
+                    icon: "🚌",
+                    link: "",
+                  },
+                  {
+                    title: "DMZ 155마일 행진",
+                    description:
+                      "NCMN 통일을 여는 DMZ 155마일 기도행진에 참여합니다.",
+                    icon: "🚶",
+                    link: "https://forms.gle/Kpos1VmM2hfM6LEr6",
+                  },
+                ].map((slide, index) => (
+                  <SwiperSlide key={index}>
+                    <div className="bg-white/5 backdrop-blur-sm p-4 rounded-lg min-h-[180px] flex flex-col">
+                      <div className="flex items-center gap-3 mb-2">
+                        <span className="text-2xl">{slide.icon}</span>
+                        <h4 className="font-bold">{slide.title}</h4>
+                      </div>
+                      <p className="text-sm text-gray-600 mb-4">
+                        {slide.description}
+                      </p>
+                      <div className="mt-auto flex justify-center w-full">
+                        <button
+                          onClick={() => {
+                            if (slide.link) {
+                              window.open(slide.link, "_blank");
+                            } else {
+                              alert("아직 신청기간이 아닙니다.");
+                            }
+                          }}
+                          className="bg-red-500 text-white py-2 px-4 rounded-full 
+                                   hover:bg-red-600 transition-colors duration-300 
+                                   text-sm font-bold"
+                        >
+                          참가 신청
+                        </button>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </div>
+        </div>
         <Swiper
           loop={true}
           speed={800}
@@ -984,7 +1166,6 @@ export default function Home() {
             bulletActiveClass:
               "swiper-pagination-bullet-active custom-bullet-active",
           }}
-          navigation={true}
           modules={[Autoplay, Pagination, Navigation]}
           className="w-[90%] md:w-[800px] mx-auto [&_.swiper-pagination-bullet-active]:!bg-red-500 [&_.swiper-pagination-bullet]:!bg-white"
           breakpoints={{
